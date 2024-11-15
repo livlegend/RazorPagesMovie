11f47c98d783b9ab7631a1701a7957deb5600e3f
Livingstone <livsenyo@gmail.com>
2024-11-15 09:18:54 -0500

    movies Project

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace RazorPagesMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public IActionResult Welcome(string name, int numtimes=1)
+        {
+            ViewData["name"] = name;
+            ViewData["numtimes"] = numtimes;
+            return View();  
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -25,6 +25,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
